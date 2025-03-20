5f2c0daf0b9c25a00f2a828d3b69b8863b6d341e
Joao Medeiros <jv.medeiros.gallina@gmail.com>
2025-03-19 23:20:31 -0300

    feat: project update

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,11 @@ app.use(cookieSession({
   httpOnly: true
 }));
 
-// Serve static files
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Register API routes
 app.use(routes);
-app.use(testRoutes); // Adicionando rotas de teste
+app.use(testRoutes);
 
-// Serve frontend for job search
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
@@ -40,7 +37,6 @@ app.get('/jobs', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../public/jobs.html'));
 });
 
-// Error handling middleware
 app.use((err: Error, req: Request, res: Response, next: Function) => {
   console.error(err.stack);
   res.status(500).json({
@@ -49,7 +45,6 @@ app.use((err: Error, req: Request, res: Response, next: Function) => {
   });
 });
 
-// Start the server
 const PORT = config.port;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
